29d21753097dc6f403ed607296bd23a72a579a39
6godpro <erickson5115@gmail.com>
2024-10-18 20:49:01 +0100

    deleted useless import

--- portfolio website/Frontend/src/main.tsx
+++ portfolio website/Frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
 // import './index.css'
 
 
