3cff7aa17fff539a8dca3908c05eae7a54cc85ca
6godpro <erickson5115@gmail.com>
2025-10-07 14:50:46 +0100

    fix: prettier formatting

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,23 +1,24 @@
-import './App.css'
-import About from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Navigation from "./components/Navigation"
-import Projects from './components/Projects'
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navigation from "./components/Navigation";
+import Projects from "./components/Projects";
+import {ToastContainer} from "react-toastify";
 
 function App() {
-
   return (
     <>
       <Navigation />
       <Hero />
       <About />
       <Projects />
-      <Contact/>
+      <Contact />
       <Footer />
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
