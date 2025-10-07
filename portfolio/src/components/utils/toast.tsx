c462972e1e14d3efe3bbbdbd07f045045a1c3a56
6godpro <erickson5115@gmail.com>
2025-10-07 14:59:06 +0100

    feat: set up custom dynamic toast notifications

---
+++ portfolio/src/components/utils/toast.tsx
@@ -0,0 +1,25 @@
+import { toast } from "react-toastify";
+import type { Id, TypeOptions } from "react-toastify";
+import ToastMessage from "../ui/ToastMessage";
+
+export const notifyLoading = (title: string, content: string): Id =>
+  toast.loading(<ToastMessage title={title} content={content} />, {
+    position: "bottom-right",
+    ariaLabel: title.toLowerCase(),
+  });
+
+
+export const updateNotify = (
+  id: Id,
+  title: string,
+  content: string,
+  type: TypeOptions
+) =>
+  toast.update(id, {
+    render: <ToastMessage title={title} content={content} />,
+    position: "bottom-right",
+    ariaLabel: title.toLowerCase(),
+    type,
+    isLoading: false,
+    autoClose: 5000,
+  });
