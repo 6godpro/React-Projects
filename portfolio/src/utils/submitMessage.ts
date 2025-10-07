240f15f1d261c2929d033cbfd65df2713e89e03e
6godpro <erickson5115@gmail.com>
2025-10-07 16:43:05 +0100

    fix: moved notification logic

--- portfolio/src/utils/submitMessage.ts
+++ portfolio/src/utils/submitMessage.ts
@@ -1,4 +1,3 @@
-import { notifyLoading, updateNotify } from "../components/utils/toast";
 import emailjs from "emailjs-com";
 
 interface MessageProps {
@@ -8,34 +7,12 @@ interface MessageProps {
 }
 
 const sendMessage = async (data: MessageProps) => {
-  const id = notifyLoading(
-    "Sending Message",
-    "Your message is being delivered."
+  await emailjs.send(
+    "service_ichkf3d",
+    "template_9mv0bjq",
+    { from_email: data.email, message: data.message },
+    "8fyGZVkg62uXJ5QNP"
   );
-
-  await emailjs
-    .send(
-      "service_ichkf3d",
-      "template_9mv0bjq",
-      { from_email: data.email, message: data.message },
-      "8fyGZVkg62uXJ5QNP"
-    )
-    .then(() => {
-      updateNotify(
-        id,
-        "Message sent",
-        "Your message was sent successfully!",
-        "success"
-      );
-    })
-    .catch(() => {
-      updateNotify(
-        id,
-        "Message not delivered",
-        "There was an error in delivering your message.",
-        "error"
-      );
-    });
 };
 
 export default sendMessage;
