f2c957fd7023b72e448851fcbfecdb03d8382823
6godpro <erickson5115@gmail.com>
2025-10-08 10:44:14 +0100

    fix: used screen size detection hook to render side navigation for smaller devices

--- portfolio/src/components/Navigation.tsx
+++ portfolio/src/components/Navigation.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import { MdOutlineMenu, MdClose } from "react-icons/md";
 import "@/assets/styles/navigation.css";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { useMobile } from "@/hooks/useMobile";
 
 const Navigation = () => {
   const [activeSection, setActiveSection] = useState("home");
   const navLinks = ["home", "about", "projects", "contact"];
   const [open, setOpen] = useState(false);
+  const isMobile = useMobile();
 
   const toggleSideNav = () => {
     setOpen(!open);
   };
 
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
     const navbar = document.getElementById("navigation");
     const rect = navbar?.getBoundingClientRect();
@@ -57,11 +59,7 @@ const Navigation = () => {
       <div className="logo">&lt;Elijah /&gt;</div>
       <nav className="navbar">
         {navLinks.map((id) => (
-          <a
-            key={id}
-            href={`#${id}`}
-            onClick={handleClick}
-          >
+          <a key={id} href={`#${id}`} onClick={handleClick}>
             {id.charAt(0).toUpperCase() + id.slice(1)}
             <span
               className={`hover-animation ${
@@ -71,39 +69,41 @@ const Navigation = () => {
           </a>
         ))}
       </nav>
-      <div className="mobile-nav">
-        <MdOutlineMenu size={24} onClick={toggleSideNav} />
-        {open && <div className="backdrop show" onClick={toggleSideNav} />}
-        <div className={`side-nav ${open ? "open" : ""}`}>
-          <div className="side-nav-header">
-            <div className="side-nav-logo">&lt;Elijah /&gt;</div>
-            <MdClose size={24} onClick={toggleSideNav} />
-          </div>
-          <nav>
-            {navLinks.map((id) => (
+      {isMobile && (
+        <div className="mobile-nav">
+          <MdOutlineMenu size={24} onClick={toggleSideNav} />
+          {open && <div className="backdrop show" onClick={toggleSideNav} />}
+          <div className={`side-nav ${open ? "open" : ""}`}>
+            <div className="side-nav-header">
+              <div className="side-nav-logo">&lt;Elijah /&gt;</div>
+              <MdClose size={24} onClick={toggleSideNav} />
+            </div>
+            <nav>
+              {navLinks.map((id) => (
+                <a
+                  key={id}
+                  href={`#${id}`}
+                  className={activeSection === id ? "active" : ""}
+                  onClick={handleClick}
+                >
+                  {id.charAt(0).toUpperCase() + id.slice(1)}
+                </a>
+              ))}
+            </nav>
+            <div className="side-nav-socials">
               <a
-                key={id}
-                href={`#${id}`}
-                className={activeSection === id ? "active" : ""}
-                onClick={handleClick}
+                href="https://www.linkedin.com/in/elijah-eromosele-146b5923b"
+                target="_blank"
               >
-                {id.charAt(0).toUpperCase() + id.slice(1)}
+                <FaLinkedinIn />
               </a>
-            ))}
-          </nav>
-          <div className="side-nav-socials">
-            <a
-              href="https://www.linkedin.com/in/elijah-eromosele-146b5923b"
-              target="_blank"
-            >
-              <FaLinkedinIn />
-            </a>
-            <a href="https://www.github.com/6godpro" target="_blank">
-              <FaGithub />
-            </a>
+              <a href="https://www.github.com/6godpro" target="_blank">
+                <FaGithub />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
