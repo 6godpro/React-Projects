7b408c4451492211b041be6d4cad0eda6b17251b
6godpro <erickson5115@gmail.com>
2025-10-09 17:06:38 +0100

    fix: move styles to css file

--- portfolio/src/components/ui/Header.tsx
+++ portfolio/src/components/ui/Header.tsx
@@ -1,21 +1,14 @@
-import '@/assets/styles/header.css'
+import "@/assets/styles/header.css";
 
 interface Props {
   content: string;
 }
 
-const Header = ({content}: Props) => {
+const Header = ({ content }: Props) => {
   return (
     <div className="header">
       <h1>{content}</h1>
-      <div
-        style={{
-          width: "150px",
-          height: "4px",
-          background: "#ededed",
-          margin: "0 auto",
-        }}
-      />
+      <div id="underline" />
     </div>
   );
 };
